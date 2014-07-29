2a0ddb78d481e7d735535ca15f84fa098f887e0f
Nikita Ivaniushchenko <nikita@ngti.nl>
2014-07-29 12:32:55 +0200

    Initial commit

---
+++ InputAccessorySample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InputAccessorySample
+//
+//  Created by Nikita Ivaniushchenko on 7/29/14.
+//  Copyright (c) 2014 NGTI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InputAccessorySample/CustomInputAccessoryView.h
@@ -0,0 +1,13 @@
+//
+//  CustomInputAccessoryView.h
+//  InputAccessorySample
+//
+//  Created by Nikita Ivaniushchenko on 7/29/14.
+//  Copyright (c) 2014 NGTI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomInputAccessoryView : UIView
+
+@end
